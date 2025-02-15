5b158291d9efac569265962e19784bd7f9a1d11c
Brendan Fattig <brendan@fattig.net>
2025-02-14 19:11:53 -0600

    Format

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Create an array called `a` with at least 100 elements in it.
-    let a = [0; 101]; 
+    let a = [0; 101];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
